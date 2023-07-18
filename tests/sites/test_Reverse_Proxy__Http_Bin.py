ec7f73798c2a36bee794cfe0b5f02ac73ae2afdd
Dinis Cruz <dinis.cruz@owasp.org>
2023-07-18 18:10:12 +0100

    Fixed test

--- tests/sites/test_Reverse_Proxy__Http_Bin.py
+++ tests/sites/test_Reverse_Proxy__Http_Bin.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-from osbot_utils.utils.Misc import list_set
+
+from osbot_utils.utils.Dev import pprint
+from osbot_utils.utils.Misc import list_set, list_contains, list_contains_list
 
 from serverless_reverse_proxy.sites.Reverse_Proxy__Http_Bin import Reverse_Proxy__Http_Bin, TARGET_SITE__HTTP_BIN
 
@@ -31,10 +33,8 @@ class test_Reverse_Proxy__Http_Bin(TestCase):
         headers      = {}
         response     = self.rp_http_bin.request_post(f'/post?{query_string}', post_data=post_data, headers=headers)
 
+        assert list_contains_list(list_set(response.get('headers')), ['Connection', 'Content-Length', 'Content-Type', 'Date', 'Server'] )
         assert list_set(response)                == ['content', 'content_type', 'headers', 'json', 'status_code', 'text']
-        assert list_set(response.get('headers')) == ['Access-Control-Allow-Credentials', 'Access-Control-Allow-Origin',
-                                                     'Connection', 'Content-Length', 'Content-Type',
-                                                     'Date', 'Server']
         assert list_set(response.get('json'   )) == ['args', 'data', 'files', 'form', 'headers', 'json', 'origin', 'url']
         assert response.get('json').get('args' ) == {'aaaa': '123', 'bbb': 'aaa'}
         assert response.get('json').get('data' ) == 'post=data&goes=here'
@@ -46,10 +46,8 @@ class test_Reverse_Proxy__Http_Bin(TestCase):
                    'aaaa'         : 'bbbb'                             }
         response = self.rp_http_bin.request_post(f'/post?{query_string}', post_data=post_data, headers=headers)
 
+        assert list_contains_list(list_set(response.get('headers')), ['Connection', 'Content-Length', 'Content-Type', 'Date', 'Server'] )
         assert list_set(response)                == ['content', 'content_type', 'headers', 'json', 'status_code', 'text']
-        assert list_set(response.get('headers')) == ['Access-Control-Allow-Credentials', 'Access-Control-Allow-Origin',
-                                                     'Connection', 'Content-Length', 'Content-Type',
-                                                     'Date', 'Server']
         assert list_set(response.get('json'   )) == ['args', 'data', 'files', 'form', 'headers', 'json', 'origin', 'url']
         assert response.get('json').get('args' ) == {'form': 'urlencoded', 'now': 'with'}
         assert response.get('json').get('data' ) == ''
